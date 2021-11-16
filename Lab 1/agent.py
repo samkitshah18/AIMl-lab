5c25285d2957a76c5d82724d18a49c4f4d6f1d42
samkitshah18 <samkitvshah18@gmail.com>
2021-11-16 10:10:38 +0530

    LAB - 3,4,5 Added

--- Lab 1/agent.py
+++ Lab 1/agent.py
@@ -5,6 +5,8 @@ import datetime
 import webbrowser
 import os
 import requests
+from datetime import datetime, timedelta
+import time
 
 
 engine = pyttsx3.init('sapi5')
@@ -41,95 +43,108 @@ def speak(audio):
     engine.runAndWait()
 
 def turnonlight():
-    speak("Lights turned on !") 
+    print("Lights turned on !") 
 
 def turnofflight():
-    speak("Lights turned off !") 
+    print("Lights turned off !") 
 
 def dimlight():
-    speak("Lights dimmed !") 
+    print("Lights dimmed !") 
 
 def brightenlight():
-    speak("Lights brightened !") 
+    print("Lights brightened !") 
  
 
 
 if __name__ == '__main__':
-    clear = lambda: os.system('cls')
-    clear()
+    # clear = lambda: os.system('cls')
+    # clear()
     
-    speak("Hello! How may I help you ?")
+    # speak("Hello! How may I help you ?")
 
-    hour = int(datetime.datetime.now().hour)
-    if hour>= 0 and hour<8:
-        turnofflight()
+    # hour = int(datetime.datetime.now().hour)
+    # if hour>= 0 and hour<8:
+    #     turnofflight()
 
-    elif hour>= 8 and hour<12:
-        turnonlight()
+    # elif hour>= 8 and hour<12:
+    #     turnonlight()
 
-    elif hour>= 12 and hour<18:
-        dimlight()  
+    # elif hour>= 12 and hour<18:
+    #     dimlight()  
 
-    else:
-        brightenlight()
+    # else:
+    #     brightenlight()
      
     while True:
          
-        command = takeCommand().lower()  
-        matches = ["site", "website", "webpage"]
-        exit = ["sleep","exit","stop","bye","goodbye"]
-        lights = ["light","adjust","change","adjustment"]
+        # command = takeCommand().lower()  
+        # matches = ["site", "website", "webpage"]
+        # exit = ["sleep","exit","stop","bye","goodbye"]
+        # lights = ["light","adjust","change","adjustment"]
  
-        if 'time' in command:
-            strTime = datetime.datetime.now().strftime("%H %M %S")   
-            speak(f"Sir, the time is {strTime}")
+        # if 'time' in command:
+        #     strTime = datetime.datetime.now().strftime("%H %M %S")   
+        #     speak(f"Sir, the time is {strTime}")
 
-        elif any(x in command for x in matches):
-            speak('Speak the name of the website')
-            website = takeCommand()
-            webbrowser.open(website)
+        # elif any(x in command for x in matches):
+        #     speak('Speak the name of the website')
+        #     website = takeCommand()
+        #     webbrowser.open(website)
 
  
-        elif "weather" in command:
-            url = "https://community-open-weather-map.p.rapidapi.com/weather"
-            speak("Please speak the city name")
-            city = takeCommand()
+        # elif "weather" in command:
+        url = "https://community-open-weather-map.p.rapidapi.com/weather"
+        # speak("Please speak the city name")
+        city = 'Mumbai'
+        print(city)
+        querystring = {"q":city+",in","lat":"0","lon":"0","id":"2172797","lang":"null","units":"metric","mode":"JSON"}      
+        headers = {
+            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+            'x-rapidapi-key': "8e5b5c251amshc212add6414d43cp14c4c0jsn5ddd339d73e6"
+            }
 
-            querystring = {"q":city+",in","lat":"0","lon":"0","id":"2172797","lang":"null","units":"metric","mode":"JSON"}      
-            headers = {
-                'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-                'x-rapidapi-key': "8e5b5c251amshc212add6414d43cp14c4c0jsn5ddd339d73e6"
-                }
+        try:
 
-            try:
-
-                response = requests.request("GET", url, headers=headers, params=querystring)
-            # print(response.text)
-                x = response.text
-                x = json.loads(x)
-                print(f"The weather in {city} is {x['weather'][0]['main']}, the temperature is {x['main']['temp']} and it feels like {x['main']['feels_like']} as the temperature ranges from {x['main']['temp_min']} to {x['main']['temp_max']} with humidity of {x['main']['humidity']}")
-                speak(f"The weather in {city} is {x['weather'][0]['main']}, the temperature is {x['main']['temp']} and it feels like {x['main']['feels_like']} as the temperature ranges from {x['main']['temp_min']} to {x['main']['temp_max']} with humidity of {x['main']['humidity']}")
-            except:
-                speak("Unable to fetch weather details")
-        
-        elif any(x in command for x in lights):
-            speak("What adjustment do you want to make ?")
-            speak("Turn on the lights, turn off the lights, dim the light, brighten the light")
-            print("Turn on the lights, turn off the lights, dim the light, brighten the light")
-
-            query = takeCommand()
-            if "dim" in query:
-                dimlight()
-            elif "brighten" in query:
-                brightenlight()
-            elif "on" in query:
+            response = requests.request("GET", url, headers=headers, params=querystring)
+        # print(response.text)
+            x = response.text
+            x = json.loads(x)
+            print(f"The weather in {city} is {x['weather'][0]['main']}, the temperature is {x['main']['temp']} and it feels like {x['main']['feels_like']} as the temperature ranges from {x['main']['temp_min']} to {x['main']['temp_max']} with humidity of {x['main']['humidity']}")
+            # speak(f"The weather in {city} is {x['weather'][0]['main']}, the temperature is {x['main']['temp']} and it feels like {x['main']['feels_like']} as the temperature ranges from {x['main']['temp_min']} to {x['main']['temp_max']} with humidity of {x['main']['humidity']}")
+            if(x['weather'][0]['main'] == "Clouds"):
                 turnonlight()
-            elif "off" or "of" in query:
+            elif(x['weather'][0]['main'] == "Clear"):
                 turnofflight()
+            elif(x['weather'][0]['main'] == "Haze"):
+                brightenlight()
+        except:
+            speak("Unable to fetch weather details")
+        dt = datetime.now() + timedelta(seconds=10)
+        # dt = dt.replace(minute=10)
+        count = 1
+        while datetime.now() < dt:
+            print(count)
+            count+=1
+            time.sleep(1)
+        
+        # elif any(x in command for x in lights):
+        #     speak("What adjustment do you want to make ?")
+        #     speak("Turn on the lights, turn off the lights, dim the light, brighten the light")
+        #     print("Turn on the lights, turn off the lights, dim the light, brighten the light")
+
+        #     query = takeCommand()
+        #     if "dim" in query:
+        #         dimlight()
+        #     elif "brighten" in query:
+        #         brightenlight()
+        #     elif "on" in query:
+        #         turnonlight()
+        #     elif "off" or "of" in query:
+        #         turnofflight()
             
 
-        elif any(x in command for x in exit):
-            speak("Bye, Have a good day !")
-            break
+        # elif any(x in command for x in exit):
+        #     speak("Bye, Have a good day !")
+        #     break
              
  
